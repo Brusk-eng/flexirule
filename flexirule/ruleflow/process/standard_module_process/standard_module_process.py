27000f81fc0e393a91c86b87f8db1bac3aaf448b
Abdo Ruzaqi <ruzaqi@gmail.com>
2026-01-01 19:49:58 +0300

    Process Doctype fixes and validation and synch file back Process record

---
+++ flexirule/ruleflow/process/standard_module_process/standard_module_process.py
@@ -0,0 +1,15 @@
+# Copyright (c) 2026, Abdo Ruzaqi and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe import _
+
+
+def execute(context, operation=None, config=None):
+	"""
+	Execute process logic here.
+	:param context: Execution context (frappe._dict)
+	:param operation: The operation to execute
+	"""
+	data = []
+	return data
