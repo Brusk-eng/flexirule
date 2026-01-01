81b9ae337c30557e300600d0072a726a20c7463b
Abdo Ruzaqi <ruzaqi@gmail.com>
2026-01-01 19:49:58 +0300

    Process Doctype fixes and validation and synch file back Process record

---
+++ flexirule/ruleflow/doctype/process/boilerplate/controller.js
@@ -0,0 +1,15 @@
+// Copyright (c) {year}, {app_publisher} and contributors
+// For license information, please see license.txt
+
+frappe.provide("flexirule.processes");
+
+flexirule.processes["{name}"] = {{
+    operations: [
+        /* func_name:function(){
+        return [
+    	
+        ]
+        }*/
+
+    ]
+}};
